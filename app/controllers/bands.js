b843a1333ee527e0affa2d8fa7da8ca03e93dd8e
remadex <s.cardon@outlook.com>
2022-06-21 15:01:08 +0200

    controller exo

--- app/controllers/bands.js
+++ app/controllers/bands.js
@@ -1,3 +1,4 @@
+/* eslint-disable ember/no-computed-properties-in-native-classes */
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
---
+++ app/controllers/bands/band/songs.js
@@ -0,0 +1,50 @@
+/* eslint-disable ember/no-computed-properties-in-native-classes */
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { empty } from '@ember/object/computed';
+
+export default class BandsBandSongsController extends Controller {
+  @service store;
+  @service router;
+
+  @tracked
+  isAddingSong = false;
+  @tracked selectSong = {
+    title: '',
+  };
+
+  @empty('selectSong.title') isAddButtonDisabled;
+
+  @action
+  addingSong() {
+    this.isAddingSong = true;
+  }
+
+  @action
+  async addSong(e) {
+    e.preventDefault();
+    console.log(this.selectSong);
+
+    let record = await this.store.createRecord('song', {
+      title: this.selectSong.title,
+      rating: 0,
+      band: this.model,
+    });
+    await record.save();
+
+    this.isAddingSong = false;
+    this.selectSong = {
+      name: '',
+    };
+  }
+
+  @action
+  cancelSong() {
+    this.selectSong = {
+      name: '',
+    };
+    this.isAddingSong = false;
+  }
+}
